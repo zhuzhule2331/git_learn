6a35fe9d82e88d2cb576e47c9b040d0e407d2e1b
jiayu.zang.ext <jiayu.zang.ext@siemens-healthineers.com>
2026-02-12 11:36:43 +0800

    gee_message_from_ros——change

--- utlis.py
+++ utlis.py
@@ -51,8 +51,9 @@ def get_installed_packages():
         return {}
 
 def get_message_from_ros():
-    a  =  3
-    return a
+    a  =  344
+    c = 5
+    return a+c
 
 # 调用并打印结果
 if __name__ == "__main__":
